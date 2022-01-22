d1c2e031c02e3eef695799cb548986f79139e653
jacob <jek82@nau.edu>
2022-01-22 12:41:34 -0600

    adding finshed broken app

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,44 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
+const ExpressError = require('./expressError');
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
+// app.post('/', function(req, res, next) {
+//   try {
+//     let results = req.body.developers.map(async d => {
+//       return await axios.get(`https://api.github.com/users/${d}`);
+//     });
+//     let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+//     return res.send(JSON.stringify(out));
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+app.get('/users/:username' , async function(req , res , next) {
+  try{
+    let resp = await axios.get(`https://api.github.com/users/${req.params.username}`)
+    if (resp.data.name === null){
+      throw new ExpressError('User not found' , 404);
+    }
+    let userData = ({name : resp.data.name , bio: resp.data.bio})
+    return res.json(userData);
+  }catch (err) {
+    return next(err)
   }
-});
+})
 
-app.listen(3000);
+
+app.use(function(err , req , res , next) {
+  let status = err.status || 500;
+  let message = err.message;
+  return res.status(status).json({
+    error: {message , status}
+  })
+})
+
+app.listen(3000 , function () {
+  console.log('App on port 3000');
+});
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,10 @@
 # Broken App Issues
+
+No .gitignore file created
+Used let instead of const for axios variable
+Use var for app variable
+App.listen not completed
+App.use error function not implemented at bottom of app.js 
+Error class not created
+Error not defined on the catch of the post route
+Error not being returned on catch route
